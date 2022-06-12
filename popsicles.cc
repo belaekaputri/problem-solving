0d8317365d1abe0740ec25e7ab9b1d8e94fc4570
belaekaputri <belaekaputri28@gmail.com>
2022-06-12 08:43:55 +0700

    commit pertama

---
+++ popsicles.cc
@@ -0,0 +1,47 @@
+/*
+&copy; SOLOLEARN
+
+Popsicles
+You have a box of popsicles and you want to give them all away to a group of
+brothers and sisters. If you have enough left in the box to give them each an even amount
+you should go fot it! if not, they will fight over them and you should eat them yourself later.
+
+Task
+Given the number of siblings that you are giving of popsicles to, determine if you
+can give them each an even amount or if you shouldn't mention the popsicles at all
+
+Input Format
+Two integer values, the first one repesent the number of siblings, and the second one
+represents the number of popsicles that you have left in the box
+
+Output Format
+A string that you says 'give away' if you are giving them away, or 'eat the yourself' if you
+will be eating them yourself
+
+Sample Input
+3 9
+
+Sample output
+give away
+
+Explanation 
+You can give the popsicles to the brothers and sisters because they would each
+get the same amount,3
+
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int siblings,popsicles;
+    string description;
+    cin>>siblings>>popsicles;
+     if (popsicles%siblings==0)
+        {
+          description= "give away";
+        }else{
+          description= "eat them yourself";
+        }
+    cout<<ket;
+    return 0;
+}
---
+++ popsicles.java
@@ -0,0 +1,48 @@
+/*
+&copy; SOLOLEARN
+
+Popsicles
+You have a box of popsicles and you want to give them all away to a group of
+brothers and sisters. If you have enough left in the box to give them each an even amount
+you should go fot it! if not, they will fight over them and you should eat them yourself later.
+
+Task
+Given the number of siblings that you are giving of popsicles to, determine if you
+can give them each an even amount or if you shouldn't mention the popsicles at all
+
+Input Format
+Two integer values, the first one repesent the number of siblings, and the second one
+represents the number of popsicles that you have left in the box
+
+Output Format
+A string that you says 'give away' if you are giving them away, or 'eat the yourself' if you
+will be eating them yourself
+
+Sample Input
+3 9
+
+Sample output
+give away
+
+Explanation 
+You can give the popsicles to the brothers and sisters because they would each
+get the same amount,3
+
+
+*/
+import java.util.Scanner;
+
+Public class Program{
+    public static void main(String[]args){
+        Scanner input= new Scanner(System.in);
+        int siblings= input.nextInt();
+        int popsicles=input.nextInt();
+        String description;
+        if (popsicles%siblings==0)
+        {
+          description= "give away";
+        }else{
+          description= "eat them yourself";
+        }
+    }
+}
